ce591ef6c4e6684d7da616c5d20e225aaea63ed7
Jason Mullings <wjasonmullings@gmail.com>
2022-07-03 23:10:22 -0400

    sidebar set up with some styling and functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [toggle, setToggle] = useState('close');
+
+  const onMenuClick =(toggleState) => toggleState === 'close' ? setToggle('open'): setToggle('close')
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    
+      <div className='container'>
+        <div className={`navigation ${toggle}`}>
+          <div className='menu-toggle'
+            onClick={()=> onMenuClick(toggle)}
+        ></div>
+        <ul className='list'>
+       
+            <li className='list-item'
+          
+          >
+            <a href='#'>
+              <span className='icon'>
+                <ion-icon name='alert-outline'></ion-icon>
+              </span>
+              <span className='text'>About</span>
+            </a>
+            </li>
+            <li className='list-item'
+           
+          >
+            <a href='#'>
+              <span className='icon'>
+                <ion-icon name='call-outline'></ion-icon>
+              </span>
+              <span className='text'>Contact</span>
+            </a>
+            </li>
+            <li className='list-item'
+           
+          >
+            <a href='#'>
+              <span className='icon'>
+                <ion-icon name='grid-outline'></ion-icon>
+              </span>
+              <span className='text'>Portfolio</span>
+            </a>
+          </li>
+         
+          </ul>
+  </div>
+  </div>
+   
   );
 }
 
